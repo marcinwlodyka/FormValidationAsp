f916f3fa1240eb38aa106d083084489aff91db55
marcin.wlodyka <marcin.wlodyka@HK-14>
2023-10-29 16:45:33 +0100

    Add project files.

---
+++ WebApplication1/Controllers/AccountController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers;
+
+public class AccountController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Register(Account account)
+    {
+        Console.WriteLine(account);
+
+        return View("Index");
+    }
+}
---
+++ WebApplication1/Models/AccountModel.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models
+{
+    
+
+    public class Account
+    {
+        public enum CityEnum
+        {
+            Krakow,
+            Warsow,
+            Paris,
+            London,
+            NewYork
+        }
+
+        [Required]
+        [MinLength(2)]
+        public string FirstName { get; set; }
+        [Required]
+        [MinLength(2)]
+        public string Surname { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        [MinLength(8)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [Required]
+        [MinLength(8)]
+        [Compare(nameof(Password))]
+        public string ConfirmPassword { get; set; }
+        [Phone]
+        public string PhoneNumber { get; set; }
+        [Range(10, 80)]
+        public int Age { get; set; }
+        public CityEnum City { get; set; }
+    }
+}
